a2ab0d2807258c7470d338c8f040f462a63daf99
hariharan <mani.hariharan@gmail.com>
2019-03-19 13:44:21 -0500

    added hardware monitor and two queues for harware and software

--- src/server/hardware_monitor.h
+++ src/server/hardware_monitor.h
@@ -6,8 +6,110 @@
 #define HFETCH_HARDWAREMONITOR_H
 
 
-class HardwareMonitor {
+#include <src/common/enumerations.h>
+#include <src/common/data_structure.h>
 
+#include <sys/inotify.h>
+#include <thread>
+#include <future>
+#include <zconf.h>
+#include <src/common/distributed_ds/queue/DistributedMessageQueue.h>
+#include <src/common/configuration_manager.h>
+#include <src/common/macros.h>
+
+#define MAX_EVENTS 1024 /*Max. number of events to process at one go*/
+#define LEN_NAME 16 /*Assuming that the length of the filename won't exceed 16 bytes*/
+#define EVENT_SIZE  ( sizeof (struct inotify_event) ) /*size of one event*/
+#define BUF_LEN     ( MAX_EVENTS * ( EVENT_SIZE + LEN_NAME )) /*buffer to store the data of events*/
+class HardwareMonitor {
+    /* file handler per layer */
+    int num_monitors;
+    std::thread* monitor_threads;
+    std::promise<void>* monitor__exit_signal;
+    DistributedMessageQueue<Event> event_queue;
+    ServerStatus AsyncMonitorInternal(std::future<void> futureObj, const Layer* current){
+        int length, i = 0,fd,wd;
+        char buffer[BUF_LEN];
+        uint32_t watch_flags = IN_CREATE | IN_MODIFY | IN_DELETE | IN_ACCESS;
+        inotify_add_watch(fd, current->layer_loc.c_str(), watch_flags);
+        while(futureObj.wait_for(std::chrono::milliseconds(1)) == std::future_status::timeout){
+            i = 0;
+            length = static_cast<int>(read(fd, buffer, BUF_LEN ));
+
+            if ( length < 0 ) {
+                perror( "read" );
+            }
+
+            while ( i < length ) {
+                struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
+                if ( event->len ) {
+                    if ( event->mask & IN_CREATE) {
+                        if (event->mask & IN_ISDIR)
+                            printf( "The directory %s was Created.\n", event->name );
+                        else
+                            printf( "The file %s was Created with WD %d\n", event->name, event->wd );
+                    }
+
+                    if ( event->mask & IN_MODIFY) {
+                        if (event->mask & IN_ISDIR)
+                            printf( "The directory %s was modified.\n", event->name );
+                        else
+                            printf( "The file %s was modified with WD %d\n", event->name, event->wd );
+                    }
+
+                    if ( event->mask & IN_DELETE) {
+                        if (event->mask & IN_ISDIR)
+                            printf( "The directory %s was deleted.\n", event->name );
+                        else
+                            printf( "The file %s was deleted with WD %d\n", event->name, event->wd );
+                    }
+
+
+                    i += EVENT_SIZE + event->len;
+                }
+            }
+        }
+        /* Clean up*/
+        inotify_rm_watch( fd, wd );
+        close(fd);
+        return ServerStatus::SERVER_SUCCESS;
+    }
+public:
+    HardwareMonitor():num_monitors(0),event_queue("HARDWARE_QUEUE",CONF->is_server,CONF->my_server,CONF->num_servers){
+        Layer* current=Layer::FIRST;
+        while(current != nullptr){
+            if(current->io_client_type == IOClientType::POSIX_FILE){
+                num_monitors++;
+            }
+            current=current->next;
+        }
+    }
+    ServerStatus AsyncMonitor(){
+        monitor_threads = new std::thread[num_monitors];
+        monitor__exit_signal = new std::promise<void>[num_monitors];
+        Layer* current=Layer::FIRST;
+        int i = 0;
+        while(current != nullptr){
+            if(current->io_client_type == IOClientType::POSIX_FILE){
+                std::future<void> futureObj = monitor__exit_signal[i].get_future();
+                monitor_threads[i]=std::thread (&HardwareMonitor::AsyncMonitorInternal, this, std::move(futureObj),current);
+            }
+            current=current->next;
+        }
+
+        return ServerStatus::SERVER_SUCCESS;
+    }
+    std::vector<Event> fetchHardwareEvents(){
+
+    }
+    void stop(){
+        for (int i = 0; i < num_monitors; ++i) {
+            /* Issue server kill signals */
+            monitor__exit_signal[i].set_value();
+            /* wait for servers to end */
+            monitor_threads[i].join();
+        }
+    }
 };
 
 
--- src/server/server.h
+++ src/server/server.h
@@ -11,12 +11,14 @@
 #include <src/common/macros.h>
 #include <src/common/configuration_manager.h>
 #include <src/common/enumerations.h>
+#include <src/common/distributed_ds/queue/DistributedMessageQueue.h>
 
 class Server {
     size_t num_workers;
     std::shared_ptr<RPC> rpc;
     std::thread* client_server_workers,*monitor_server_workers;
     std::promise<void>* client_server_exit_signal,*monitor_server_exit_signal;
+    DistributedMessageQueue<Event> event_queue;
     ServerStatus runClientServerInternal(std::future<void> futureObj){
         while(futureObj.wait_for(std::chrono::milliseconds(1)) == std::future_status::timeout){
         }
@@ -28,7 +30,7 @@ class Server {
         return ServerStatus::SERVER_SUCCESS;
     }
 public:
-    Server(size_t num_workers_=1):num_workers(num_workers_){
+    Server(size_t num_workers_=1):num_workers(num_workers_),event_queue("APPLICATION_QUEUE",CONF->is_server,CONF->my_server,CONF->num_servers){
         rpc=Singleton<RPC>::GetInstance("RPC_SERVER_LIST",CONF->is_server,CONF->my_rank,CONF->comm_size);
     }
     ServerStatus async_run(size_t numWorker=1){
