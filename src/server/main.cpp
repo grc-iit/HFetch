38638400abe05f2841d7c6ff640d38f3c833dd1f
hdevarajan92 <hdevarajan@hawk.iit.edu>
2019-03-26 15:47:01 -0500

    remove prints

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -21,13 +21,13 @@ int main(int argc, char*argv[]){
     }
     MPI_Barrier(MPI_COMM_WORLD);*/
     InputArgs args = Server::InitializeServer(argc,argv);
+    setup_env(args);
     if(args.is_logging){
         char complete_log[256];
         sprintf(complete_log, "%s/server_%d.log", args.log_path,my_rank);
-        freopen(complete_log,"w+",stdout);
-        freopen(complete_log,"a",stderr);
+        //freopen(complete_log,"w+",stdout);
+        freopen(complete_log,"w+",stderr);
     }
-    setup_env(args);
     Singleton<Server>::GetInstance()->async_run(args.num_workers);
     if (CONF->my_rank_world == 0) {
         printf("Press any key to exit server\n");
