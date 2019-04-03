eab7b86a7c7aabde058b950da0e9047cfce3c38f
hdevarajan92 <hdevarajan@hawk.iit.edu>
2019-04-03 12:59:32 -0500

    compile problem

--- test/synthetic_benchmark/stacker_reader_pfs.cpp
+++ test/synthetic_benchmark/stacker_reader_pfs.cpp
@@ -118,7 +118,7 @@ int main(int argc, char*argv[]){
     switch(input.type){
         case ReaderType::READ_ENTIRE_EACH_TS:{
             size_t read_size = 1*1024*1024;
-            size_t read_iterations=file_size/read_size/comm_size;
+            size_t read_iterations=file_size/read_size;
             read_iterations=read_iterations==0?1:read_iterations;
             void* buf = malloc(read_size);
             for(size_t i=0;i<timesteps;++i){
@@ -149,7 +149,7 @@ int main(int argc, char*argv[]){
         }
         case ReaderType::READ_ENTIRE_SAME_TS:{
             size_t read_size = 1*1024*1024;
-            size_t read_iterations=file_size/read_size/2;
+            size_t read_iterations=file_size/read_size;
             void* buf = malloc(read_size);
             for(size_t i=0;i<timesteps;++i){
                 for(int j=0;j<read_iterations;++j){
@@ -168,7 +168,7 @@ int main(int argc, char*argv[]){
             srand(200);
 
             size_t read_size = 1*1024*1024;
-            size_t read_iterations=file_size/read_size/2;
+            size_t read_iterations=file_size/read_size;
             void* buf = malloc(read_size);
             for(size_t i=0;i<timesteps;++i){
                 for(int j=0;j<read_iterations;++j){
